d20242d3744f7c961062cb7ce6dc919a27f043c3
David Romaní <david@flux.cat>
2025-12-14 21:20:25 +0100

    load extension config/services.php into src/FluxVerifactuBundle

--- src/FluxVerifactuBundle.php
+++ src/FluxVerifactuBundle.php
@@ -2,6 +2,8 @@
 
 namespace Flux\VerifactuBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 /**
@@ -9,6 +11,11 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
  */
 final class FluxVerifactuBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.php');
+    }
+
     public function getPath(): string
     {
         return __DIR__;
