baf613e6ea7ca07d87c5b91b3766704505394510
David Romaní <david@flux.cat>
2025-12-15 20:42:13 +0100

    add Dto/ComputerSystemDto

---
+++ src/Dto/ComputerSystemDto.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flux\VerifactuBundle\Dto;
+
+final readonly class ComputerSystemDto
+{
+    public function __construct(
+        private string $vendorName,
+        private string $vendorNif,
+        private string $name,
+        private string $id,
+        private string $version,
+        private string $installationNumber,
+        private bool $onlySupportsVerifactu,
+        private bool $supportsMultipleTaxpayers,
+        private bool $hasMultipleTaxpayers
+    ) {
+    }
+
+    public function getVendorName(): string
+    {
+        return $this->vendorName;
+    }
+
+    public function getVendorNif(): string
+    {
+        return $this->vendorNif;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    public function getInstallationNumber(): string
+    {
+        return $this->installationNumber;
+    }
+
+    public function isOnlySupportsVerifactu(): bool
+    {
+        return $this->onlySupportsVerifactu;
+    }
+
+    public function isSupportsMultipleTaxpayers(): bool
+    {
+        return $this->supportsMultipleTaxpayers;
+    }
+
+    public function isHasMultipleTaxpayers(): bool
+    {
+        return $this->hasMultipleTaxpayers;
+    }
+}
