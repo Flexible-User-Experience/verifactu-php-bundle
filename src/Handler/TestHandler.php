d2e8ece3a70e5d7043c901cd2accf9d3977d1c73
David Romaní <david@flux.cat>
2025-12-16 20:02:06 +0100

    Inject `isProdEnvironment` as a dependency in TestHandler and update getTest logic accordingly

--- src/Handler/TestHandler.php
+++ src/Handler/TestHandler.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Flux\VerifactuBundle\Handler;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
 final class TestHandler
 {
+    public function __construct(
+        #[Autowire(param: '%app.is_prod_environment%')]
+        private readonly bool $isProdEnvironment
+    ) {
+    }
+
     public function getTest(): string
     {
-        return 'test';
+        return $this->isProdEnvironment ? 'prod' : 'test';
     }
 }
