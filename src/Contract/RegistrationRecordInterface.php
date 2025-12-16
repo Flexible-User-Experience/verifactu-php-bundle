528b01a8793f1304fab2ebe3cbea3171bab5280e
David Romaní <david@flux.cat>
2025-12-16 18:53:07 +0100

    Add RegistrationRecordInterface defining contract for registration record handling

---
+++ src/Contract/RegistrationRecordInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flux\VerifactuBundle\Contract;
+
+use DateTimeInterface;
+use josemmo\Verifactu\Models\Records\CorrectiveType;
+use josemmo\Verifactu\Models\Records\InvoiceIdentifier;
+use josemmo\Verifactu\Models\Records\InvoiceType;
+
+interface RegistrationRecordInterface
+{
+    public function getInvoiceIdentifier(): InvoiceIdentifier;
+
+    public function getPreviousInvoiceIdentifier(): ?InvoiceIdentifier;
+
+    public function getPreviousHash(): ?string;
+
+    public function getHash(): string;
+
+    public function getHashAt(): DateTimeInterface;
+
+    public function getIsCorrection(): bool;
+
+    public function getIsPriorRejection(): bool;
+
+    public function getIssuerName(): string;
+
+    public function getInvoiceType(): InvoiceType;
+
+    public function getOperationDate(): ?DateTimeInterface;
+
+    public function getDescription(): string;
+
+    public function getRecipients(): array;
+
+    public function getCorrectiveType(): ?CorrectiveType;
+
+    public function getCorrectiveInvoices(): array;
+
+    public function getCorrectedBaseAmount(): ?string;
+
+    public function getCorrectedTaxAmount(): ?string;
+
+    public function getReplacedInvoices(): array;
+
+    public function getBreakdown(): array;
+
+    public function getTotalTaxAmount(): string;
+
+    public function getTotalAmount(): string;
+}
