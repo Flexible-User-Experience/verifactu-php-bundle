19511f04b119ae18d22cc460f6556b7b966cab11
David Romaní <david@flux.cat>
2025-12-17 18:54:55 +0100

    Inject `kernel.default_locale` as a constructor argument for `TestHandler` in `services.php`.

--- config/services.php
+++ config/services.php
@@ -9,6 +9,9 @@ use Flux\VerifactuBundle\Handler\TestHandler;
 return static function (ContainerConfigurator $container): void {
     $container->services()
         ->set('flux_verifactu.test_handler', TestHandler::class)
+            ->args([
+                param('kernel.default_locale'),
+            ])
         ->alias(TestHandler::class, 'flux_verifactu.test_handler')
         ->public()
     ;
