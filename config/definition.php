7e41021a4abc87768c0588d9888549ea182895e8
David Romaní <david@flux.cat>
2025-12-17 19:01:07 +0100

    Add `info` and `isRequired` to `is_prod_environment` in `definition.php` configuration

--- config/definition.php
+++ config/definition.php
@@ -9,7 +9,11 @@ use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 return static function (DefinitionConfigurator $definition): void {
     $definition->rootNode()
         ->children()
-            ->booleanNode('is_prod_environment')->defaultValue(false)->end()
+            ->booleanNode('is_prod_environment')
+                ->info('Set to true in production environment to make AEAT API real calls, only when you are sure that what are you doing is correct.')
+                ->defaultValue(false)
+                ->isRequired()
+            ->end()
         ->end()
     ;
 };
