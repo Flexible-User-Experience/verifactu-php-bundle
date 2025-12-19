601f5cf3215fa1833fa7590e0f4e2b77a927b847
David Romaní <david@flux.cat>
2025-12-19 17:12:20 +0100

    Inject `aeat_client_handler` service and update `test_handler` to use `isProdEnvironment` configuration in `services.php`.

--- config/services.php
+++ config/services.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Flux\VerifactuBundle\FluxVerifactuBundle;
+use Flux\VerifactuBundle\Handler\AeatClientHandler;
 use Flux\VerifactuBundle\Handler\TestHandler;
 
 return static function (ContainerConfigurator $container): void {
     $container->services()
+        ->set('flux_verifactu.aeat_client_handler', AeatClientHandler::class)
+            ->args([
+                abstract_arg(FluxVerifactuBundle::COMPUTER_SYSTEM_CONFIG_KEY),
+            ])
+        ->alias(AeatClientHandler::class, 'flux_verifactu.aeat_client_handler')
+        ->public()
+    ;
+    $container->services()
         ->set('flux_verifactu.test_handler', TestHandler::class)
             ->args([
-                param('kernel.default_locale'),
+                abstract_arg(FluxVerifactuBundle::IS_PROD_ENVIRONMENT_CONFIG_KEY),
             ])
         ->alias(TestHandler::class, 'flux_verifactu.test_handler')
         ->public()
