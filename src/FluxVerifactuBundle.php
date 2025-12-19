bd769c393141bcecf8516c805a62ac5409fb163e
David Romaní <david@flux.cat>
2025-12-19 18:03:28 +0100

    Remove `ComputerSystemFactory` and `FiscalIdentifierFactory` dependencies from `AeatClientHandler`.

--- src/FluxVerifactuBundle.php
+++ src/FluxVerifactuBundle.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Flux\VerifactuBundle;
 
-use Flux\VerifactuBundle\Factory\ComputerSystemFactory;
-use Flux\VerifactuBundle\Factory\FiscalIdentifierFactory;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -30,9 +28,7 @@ final class FluxVerifactuBundle extends AbstractBundle
         $container->import('../config/services.php');
         $builder->getDefinition('flux_verifactu.aeat_client_handler')
             ->setArgument(0, $config[self::COMPUTER_SYSTEM_CONFIG_KEY])
-            ->setArgument(1, ComputerSystemFactory::class)
             ->setArgument(2, $config[self::FISCAL_IDENTIFIER_CONFIG_KEY])
-            ->setArgument(3, FiscalIdentifierFactory::class)
         ;
         $builder->getDefinition('flux_verifactu.test_handler')
             ->setArgument(0, $config[self::IS_PROD_ENVIRONMENT_CONFIG_KEY])
